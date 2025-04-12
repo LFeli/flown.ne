71536e7be646504ab4078b1f0af90fe733764549
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-12 19:56:25 -0300

    :sparkles: feat: integrate SiteFooter component into AppLayout for improved layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import { SiteFooter } from '@/components/layout/footer'
 import { SiteHeader } from '@/components/layout/header'
 import { AppSidebar } from '@/components/layout/sidebar/sidebar'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
@@ -14,7 +15,9 @@ export default function AppLayout({
       <SidebarInset>
         <SiteHeader />
 
-        {children}
+        <div className="flex flex-1 flex-col">{children}</div>
+
+        <SiteFooter />
       </SidebarInset>
     </SidebarProvider>
   )
