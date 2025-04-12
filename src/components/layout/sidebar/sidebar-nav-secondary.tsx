3cb7f032ca76810cd3bc8e0f2a0c2fc9fbe59af9
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-12 17:32:54 -0300

    :sparkles: feat: add AppSidebarNavSecondary component for secondary sidebar navigation

---
+++ src/components/layout/sidebar/sidebar-nav-secondary.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+import type { SidebarItem } from '@/types/sidebar'
+
+export function AppSidebarNavSecondary({
+  items,
+  ...props
+}: SidebarItem & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
+  return (
+    <SidebarGroup {...props}>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {items.map(item => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <Link href={item.url}>
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
