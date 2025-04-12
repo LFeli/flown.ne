f8726c34eb9dfe1a6ffbc1012cd4a93db2ec8ca6
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-12 10:41:37 -0300

    :sparkles: feat: enhance layout metadata and introduce site configuration

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: 'flown.ne',
+  url: 'https://github.com/LFeli/flown.ne',
+  description:
+    'Flown.ne is a flexible, modern dashboard platform designed for deep data interaction. Explore complex tables, dynamic filters, custom functions, and elegant charts—all in one intuitive interface.',
+  author: {
+    name: 'Lucas Araújo | LFeli',
+    github: 'https://github.com/LFeli',
+  },
+}
