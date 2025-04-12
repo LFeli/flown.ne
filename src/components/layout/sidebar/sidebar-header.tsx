85c13b1d124694c462118b78f6677ba31143a159
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-12 17:32:40 -0300

    :sparkles: feat: add AppSidebarHeader component for enhanced sidebar navigation

---
+++ src/components/layout/sidebar/sidebar-header.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+
+import { CrownIcon, InfinityIcon } from 'lucide-react'
+
+import {
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '../../ui/sidebar'
+
+export function AppSidebarHeader() {
+  return (
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            asChild
+            size={'lg'}
+            className="data-[slot=sidebar-menu-button]:!p-1.5"
+          >
+            <Link href={'/'}>
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-green-50">
+                <InfinityIcon className="size-4" />
+              </div>
+
+              <span className="flex flex-col">
+                <h4 className="font-semibold text-base">Flown.ne Inc.</h4>
+
+                <span className="flex gap-x-1.5 text-muted-foreground text-xs">
+                  <CrownIcon className="size-3" />
+                  <span>Premium</span>
+                </span>
+              </span>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
+  )
+}
