fa668f5c6719ec0e1c2a3effc7a8c1014a473530
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-12 17:32:27 -0300

    :lipstick: fix: suppress hydration warning in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           'min-h-svh font-sans antialiased',
