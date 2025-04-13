7e47ab12fbbc1db0ed5822de7a37f1cf015e535b
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-12 22:25:50 -0300

    :sparkles: feat: add VisitorsChartWrapper component for encapsulating visitor chart logic

---
+++ src/app/(app)/dashboard/_components/chart/chart-wrapper.tsx
@@ -0,0 +1,79 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+import type { ChartTimeRange } from '../../types'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import type { VisitorsChartData } from '@/types/chart'
+import { VisitorsChartInteractive } from './chart-interactive'
+import { chartPeriodLabel } from './config'
+
+interface VisitorsChartWrapperProps {
+  data: VisitorsChartData[]
+  timeRange: ChartTimeRange
+  onTimeRangeChange: (timeRange: ChartTimeRange) => void
+}
+
+export function VisitorsChartWrapper({
+  data,
+  timeRange,
+  onTimeRangeChange,
+}: VisitorsChartWrapperProps) {
+  return (
+    <Card className="bg-gradient-to-t from-primary/5 to-card shadow-xs">
+      <CardHeader className="relative">
+        <CardTitle>Total Visitors</CardTitle>
+
+        <CardDescription>
+          <span className="@[540px]/card:block hidden">
+            Total for the {chartPeriodLabel[timeRange].toLowerCase()}
+          </span>
+
+          <span className="@[540px]/card:hidden">
+            Last {chartPeriodLabel[timeRange].toLowerCase()}
+          </span>
+        </CardDescription>
+
+        <div className="absolute top-0 right-4">
+          <Select value={timeRange} onValueChange={onTimeRangeChange}>
+            <SelectTrigger
+              className="flex @[767px]/card:hidden w-40"
+              aria-label="Select a value"
+            >
+              <SelectValue
+                placeholder={chartPeriodLabel[timeRange].toLowerCase()}
+              />
+            </SelectTrigger>
+
+            <SelectContent className="rounded-xl">
+              <SelectItem value="90d" className="rounded-lg">
+                Last 3 months
+              </SelectItem>
+              <SelectItem value="30d" className="rounded-lg">
+                Last 30 days
+              </SelectItem>
+              <SelectItem value="7d" className="rounded-lg">
+                Last 7 days
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </CardHeader>
+
+      <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+        <VisitorsChartInteractive data={data} />
+      </CardContent>
+    </Card>
+  )
+}
