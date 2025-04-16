99707dd313ea7e02cf498ec6ed2904632050f350
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-16 19:52:57 -0300

    :sparkles: feat: enhance ContentTableFooter with pagination controls and page size selector

--- src/app/(app)/dashboard/_components/table/footer.tsx
+++ src/app/(app)/dashboard/_components/table/footer.tsx
@@ -1,3 +1,28 @@
-export function ContentTableFooter() {
-  return <span>my footer here...</span>
+import { PageSizeSelector } from '@/components/table/page-size-selector'
+import { PaginationControl } from '@/components/table/pagination-control'
+import { PaginationInfoDisplay } from '@/components/table/pagination-info-display'
+import type { Table } from '@tanstack/react-table'
+
+interface ContentTableFooterProps<TData> {
+  table: Table<TData>
+}
+
+export function ContentTableFooter<TData>({
+  table,
+}: ContentTableFooterProps<TData>) {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex w-full flex-col-reverse items-start justify-between gap-4 md:flex-row md:items-center lg:space-x-8">
+        <PageSizeSelector table={table} />
+
+        <div className="flex items-center gap-4">
+          <div className="flex items-center justify-center font-medium text-sm">
+            <PaginationInfoDisplay table={table} />
+          </div>
+
+          <PaginationControl table={table} paginationType="double" />
+        </div>
+      </div>
+    </div>
+  )
 }
