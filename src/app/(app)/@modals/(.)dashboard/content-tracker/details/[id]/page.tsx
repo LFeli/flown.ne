334259ec98535e905aa3f1ed8a5a4fb77bcb5cd0
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-18 17:26:34 -0300

    feat: refactor ContentTrackerDetailsModal to use TrackerDetailsModal component and mock data

--- src/app/(app)/@modals/(.)dashboard/content-tracker/details/[id]/page.tsx
+++ src/app/(app)/@modals/(.)dashboard/content-tracker/details/[id]/page.tsx
@@ -1,66 +1,13 @@
-'use client'
+import { TrackerDetailsModal } from '@/components/features/content-tracker/tracker-details-modal'
+import { ContentTrackerTableMock } from '@/mocks/table'
 
-import { usePathname, useRouter } from 'next/navigation'
-import React from 'react'
+export default async function ContentTrackerDetailsModal({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  const data = ContentTrackerTableMock.find(item => item.id === Number(id))
 
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-} from '@/components/ui/drawer'
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from '@/components/ui/sheet'
-
-import { useIsMobile } from '@/hooks/use-mobile'
-
-export default function ContentTrackerDetailsModal() {
-  const mobile = useIsMobile()
-  const router = useRouter()
-  const pathname = usePathname()
-
-  const isDetailsPath = pathname.includes('/dashboard/content-tracker/details/')
-  const [isOpen, setIsOpen] = React.useState<boolean>(!!isDetailsPath) // for control the modal open state based on the pathname for can make the animation smooth
-
-  function handleOnClose(open: boolean) {
-    if (!open) {
-      setIsOpen(false)
-      const timeout = setTimeout(() => router.back(), 150) // Delay to allow the modal to close, based in animate-out duration 0.15s
-
-      return () => clearTimeout(timeout)
-    }
-  }
-
-  if (mobile) {
-    return (
-      <Drawer open={isOpen} onOpenChange={handleOnClose}>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-        </DrawerContent>
-      </Drawer>
-    )
-  }
-
-  return (
-    <Sheet open={isOpen} onOpenChange={handleOnClose}>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
-      </SheetContent>
-    </Sheet>
-  )
+  return <TrackerDetailsModal data={data} />
 }
