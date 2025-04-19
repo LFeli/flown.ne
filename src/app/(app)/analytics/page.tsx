bdda8ec1b46d59edb8a44206b244fc1adb3ff5a4
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-19 17:43:22 -0300

    feat: update AnalyticsPage layout to include CampaignPerformanceTable and ClickActivityChart components

--- src/app/(app)/analytics/page.tsx
+++ src/app/(app)/analytics/page.tsx
@@ -1,8 +1,21 @@
+import { SiteFooter } from '@/components/layout/footer'
+
+import { CampaignPerformanceTable } from './_components/campaign-performance-section/campaign-performance-table'
+import { ClickActivityChart } from './_components/click-activity-over-time-section/click-activity-chart'
+import { TotalConversionsChart } from './_components/total-conversions-overview-section/total-conversions-chart'
+
 export default function AnalyticsPage() {
   return (
-    <div>
-      <h1>Analytics</h1>
-      <p>This is the analytics page.</p>
-    </div>
+    <main className="flex flex-1 flex-col gap-3 ">
+      <div className="flex h-full flex-col gap-4 p-4 md:gap-6 md:p-6">
+        <article className="md: grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6">
+          <TotalConversionsChart />
+          <ClickActivityChart />
+        </article>
+
+        <CampaignPerformanceTable />
+      </div>
+      <SiteFooter />
+    </main>
   )
 }
