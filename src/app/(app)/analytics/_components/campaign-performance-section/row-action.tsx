2032963b03423e2e61cf52da3559e56145e4bda6
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-20 19:56:49 -0300

    feat: add AnalyticCampaignPerformanceRowAction component for row action menu

---
+++ src/app/(app)/analytics/_components/campaign-performance-section/row-action.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import type { AnalyticCampaignPerformanceData } from '@/types/table'
+import type { Row } from '@tanstack/react-table'
+import { MoreVerticalIcon } from 'lucide-react'
+
+interface AnalyticCampaignPerformanceRowActionProps<
+  TData extends AnalyticCampaignPerformanceData,
+> {
+  row: Row<TData>
+}
+export function AnalyticCampaignPerformanceRowAction<
+  TData extends AnalyticCampaignPerformanceData,
+>({ row }: AnalyticCampaignPerformanceRowActionProps<TData>) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex size-8 text-muted-foreground data-[state=open]:bg-muted"
+          size="icon"
+        >
+          <MoreVerticalIcon />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className="w-32">
+        <DropdownMenuItem>Make a copy</DropdownMenuItem>
+        <DropdownMenuItem>Favorite</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
