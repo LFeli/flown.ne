9fd50acb83f141ff34e611591f6ef682f84631d5
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-20 19:57:11 -0300

    refactor: update AnalyticsPage component to remove unused props and comments

--- src/app/(app)/analytics/page.tsx
+++ src/app/(app)/analytics/page.tsx
@@ -4,7 +4,19 @@ import { CampaignPerformanceTable } from './_components/campaign-performance-sec
 import { ClickActivityChart } from './_components/click-activity-over-time-section/click-activity-chart'
 import { TotalConversionsChart } from './_components/total-conversions-overview-section/total-conversions-chart'
 
-export default function AnalyticsPage() {
+// interface AnalyticsPageProps {
+//   searchParams: Promise<{ start?: string; end?: string }>
+// }
+
+// export default async function AnalyticsPage({
+//   searchParams,
+// }: AnalyticsPageProps) {
+export default async function AnalyticsPage() {
+  // const { start, end } = await searchParams
+
+  // console.log('start', start)
+  // console.log('end', end)
+
   return (
     <main className="flex flex-1 flex-col gap-3 ">
       <div className="flex h-full flex-col gap-4 p-4 md:gap-6 md:p-6">
