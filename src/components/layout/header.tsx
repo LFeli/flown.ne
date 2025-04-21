39851864e8f1f99f3d597947f6a2488a604b26d5
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-21 13:17:31 -0300

    feat: update SiteHeader to display dynamic title based on current pathname

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,15 @@
-import { SlashIcon } from 'lucide-react'
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+import { formatToTitleCase } from '@/utils/format-to-title-case'
 import { Separator } from '../ui/separator'
 import { SidebarTrigger } from '../ui/sidebar'
 import { ThemeSwitcher } from './theme-switcher'
 
 export function SiteHeader() {
+  const pathname = usePathname().split('/')
+
   return (
     <header className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-16">
       <div className="flex w-full items-center justify-between gap-1 px-4 lg:gap-2 lg:px-6">
@@ -12,10 +18,12 @@ export function SiteHeader() {
 
           <Separator
             orientation="vertical"
-            className="mx-2 data-[orientation=vertical]:h-6"
+            className="mx-4 data-[orientation=vertical]:h-6"
           />
 
-          <h1 className="font-medium text-base">Dashboard</h1>
+          <h1 className="font-medium text-base">
+            {formatToTitleCase(pathname[1])}
+          </h1>
         </div>
 
         <ThemeSwitcher />
