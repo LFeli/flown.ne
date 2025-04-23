d68a208fca4f76e9bf18a2debf93a11f1b379ccd
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-22 22:00:45 -0300

    feat: enhance project directory columns with tooltips and status color mapping

--- src/app/(app)/projects/project-directory-section/columns.tsx
+++ src/app/(app)/projects/project-directory-section/columns.tsx
@@ -1,28 +1,63 @@
 'use client'
 
-import { Badge } from '@/components/ui/badge'
-import type { ProjectData } from '@/types/table'
 import type { ColumnDef } from '@tanstack/react-table'
+
+import { Badge } from '@/components/ui/badge'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
+import type { ProjectData, ProjectStatus } from '@/types/table'
+
+const statusColorMap: Record<
+  ProjectStatus,
+  'secondary' | 'destructive' | 'default' | 'outline'
+> = {
+  active: 'secondary',
+  draft: 'outline',
+  'in-review': 'default',
+  archived: 'destructive',
+}
 
 export const columns: ColumnDef<ProjectData>[] = [
   {
-    accessorKey: 'id',
-    header: 'Project ID',
+    accessorKey: 'description',
+    header: 'Description',
     cell: ({ row }) => {
-      const campaignId = row.original.id
+      const description = row.original.description
+
+      return (
+        <Tooltip>
+          <TooltipTrigger className="max-w-60 truncate text-muted-foreground text-sm">
+            <span className="w-full">{description}</span>
+          </TooltipTrigger>
+
+          <TooltipContent className="max-w-56 p-2">
+            {description}
+          </TooltipContent>
+        </Tooltip>
+      )
+    },
+    meta: {
+      cellClassName: 'w-3xs min-w-3xs',
+    },
+  },
+
+  {
+    accessorKey: 'status',
+    header: 'Status',
+    cell: ({ row }) => {
+      const status = row.original.status
 
       return (
         <Badge
-          variant={'secondary'}
-          className="w-full max-w-28 truncate px-1.5 font-mono text-muted-foreground"
+          variant={statusColorMap[status]}
+          className="rounded-full px-3 py-1"
         >
-          {campaignId}
+          {status.replace(/-/g, ' ')}
         </Badge>
       )
-    },
-
-    meta: {
-      cellClassName: 'w-32 min-w-32',
     },
   },
 ]
