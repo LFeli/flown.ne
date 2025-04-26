f0090f8d39c48f5bc466bbccf7ad4d33c0d710c9
Lucas Araújo <lucasfelipemk@gmail.com>
2025-04-26 00:15:51 -0300

    feat: integrate TeamListTable component into TeamWrapper for enhanced member display

--- src/app/(app)/team/_components/team-wrapper.tsx
+++ src/app/(app)/team/_components/team-wrapper.tsx
@@ -11,6 +11,7 @@ import { members, teamOptions } from '@/mocks/pages/team'
 
 import { SelectTeamOption } from './select-team-option'
 import { TeamStatsCardWrapper } from './stats-card-section/cards-wrapper'
+import { TeamListTable } from './team-list-section/team-list-table'
 
 interface TeamWrapperProps {
   option?: string
@@ -28,7 +29,7 @@ export function TeamWrapper({ option }: TeamWrapperProps) {
   console.log(teamMembers)
 
   return (
-    <Card className="gap-y-4 border-none bg-transparent py-4">
+    <Card className="gap-y-4 border-none bg-transparent py-4 shadow-none md:gap-y-8">
       <CardHeader className="relative flex flex-col items-center justify-between gap-x-8 gap-y-6 px-0 md:flex-row md:gap-x-12">
         <div className="space-y-1.5">
           <CardTitle>Team Overview</CardTitle>
@@ -42,10 +43,10 @@ export function TeamWrapper({ option }: TeamWrapperProps) {
         <SelectTeamOption selected={selectedTeam.id} teams={teamOptions} />
       </CardHeader>
 
-      <CardContent className="px-0">
+      <CardContent className="space-y-4 px-0 md:space-y-8">
         <TeamStatsCardWrapper />
 
-        <span>my table here...</span>
+        <TeamListTable members={teamMembers} />
       </CardContent>
     </Card>
   )
